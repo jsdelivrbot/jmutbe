eae521304cd7c32800fe570b9eefa67d1045caa5
Ross <ross@localhost.localdomain>
2015-03-25 01:02:02 -0400

    make it better

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
-var passport = require('passport');
 var flash    = require('connect-flash');
 
 var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
 
@@ -17,29 +15,21 @@ var configDB = require('./config/dbModule.js');
 //Configs of middleware =======================================
 mongoose.connect(configDB.url); //connect to DB
 
-require('./config/passport')(passport); //pass passport for configuration
-
 //Logs requests to console
 app.use(morgan('dev'));
 
-//Config of cookieParser
-app.use(cookieParser());
 
 //Configuration that allows usage of BodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Partial config of passport
-app.use(session({ secret: 'ilovepolishvodka' })); //session secret
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(flash());
 
 //The middleware shows express where all the static files are to be delivered.
 app.use(express.static(__dirname + '/FrontEnd'))
 
 //Load Routes =================================================
-require('./app/routes.js')(app,passport); // Loads routes and passport
+require('./app/routes.js')(app); // Loads routes and passport
 
 
 //Launch ======================================================
